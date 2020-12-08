43e7847c755b35fbb870a4edb747d8ec1e4d6d06
hotea <oanahotea18@gmail.com>
2020-12-08 20:49:43 +0200

    Updated data container

--- Sources/DataContainer.py
+++ Sources/DataContainer.py
@@ -1,15 +1,30 @@
 
 dataContainer = {
-    "Detalii pagina": {"Aceasta pagina doreste sa prezinte bla bla bla... Multumim pentru interes! Vreti sa aflati detalii "
-                      "despre o anumita masina?": ["Da", "Nu"]},
-    "Da": {"Super! Alege te rog una din categoriile de masini de mai jos: ": ["Audi", "BMW"]},
+    "Detalii pagina": {"Aceasta pagina prezinta informatii despre toate modelele de Audi. Multumim pentru interes! Vreti sa aflati detalii "
+                      "despre un anumit model?": ["Da", "Nu"]},
+    "Da": {"Super! Alege te rog una din categoriile de modele de mai jos: ": ["A1", "A2", "A3", "A4","A5","A6","A7","A8"]},
     "Nu": "Ok, in cazul in care va razganditi puteti cere informatii oricand prin accesarea intrebariilor predefinite "
-           "accesand butonul 'Detalii masini'. Va multumim!",
-    "Detalii masini": {"Va rog sa alegeti marca masinii dorite": ["Audi", "BMW"]},
-    "Audi": {"Va rog sa selectati modelul dorit": ["A4", "A6"]},
-    "A4": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Detalii masini", "Vreau sa vad cele mai faine masini"],
-                "images": [r"D:\80_workspace\30_Python\ChatBot_TI\Images\00936.png", r"D:\80_workspace\30_Python\ChatBot_TI\Images\00977.png"]},
-    "Vreau sa vad cele mai faine masini": {"Acestea sunt cele mai frumoase masini de pe pagina": [], "images": [r"D:\80_workspace\30_Python\ChatBot_TI\Images\00936.png"]},
-    "Vreau sa vad cele mai faine...": {"Acestea sunt cele mai frumoase masini de pe pagina": [], "images": [r"D:\80_workspace\30_Python\ChatBot_TI\Images\00936.png"]},
-    "Vreau sa vad cele ma...": {"Acestea sunt cele mai frumoase masini de pe pagina": [], "images": [r"D:\80_workspace\30_Python\ChatBot_TI\Images\00936.png"]}
+           "accesand butonul 'Modele masini'. Va multumim!",
+    "Modele masini": {"Va rog sa selectati modelul dorit": ["A1", "A2", "A3", "A4","A5","A6","A7","A8"]},
+    "A1": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini", "Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a1\40087655.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a1\30015783.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a1\180758144.jpg"]},
+	"A2": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini", "Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a2\664506709.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a2\680700195.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a2\1930496441.jpg"]]},
+	"A3": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini", "Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a3\10364498.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a3\224078563.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a3\541562506.jpg"]]},
+	"A4": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini", "Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a4\244124874.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a4\264101116.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a4\370360074.jpg"]]},
+	"A5": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini", "Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a5\17119867.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a5\51970054.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a5\52612623.jpg"]]},
+	"A6": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini", "Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a6\499069.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a6\3800181.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a6\56749134.jpg"]]},
+	"A7": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini", "Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a7\306876691.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a7\365462984.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a7\444790448.jpg"]]},
+	"A8": {" . Cu ce va mai pot ajuta?": ["Detalii pagina", "Modele masini","Notificare model nou"],
+                "images": [r"D:\TI-Chatbot\Sources\resources\Audi\a8\1305244055.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a8\1327057123.jpg", r"D:\TI-Chatbot\Sources\resources\Audi\a8\1329193042.jpg"]]},
+   "Notificare model nou": {"Doriti sa primiti o notificare cand apare un model nou?": ["Da, doresc", "Nu!"]},
+   "Da, doresc": {TBD},
+   "Nu!" : "Ok, in cazul in care va razganditi puteti cere informatii oricand prin accesarea intrebariilor predefinite "
+           "accesand butonul 'Modele masini'. Va multumim!"
+   
 }
