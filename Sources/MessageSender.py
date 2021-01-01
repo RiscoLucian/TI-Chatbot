6dcdce62a49a0cbe91651f30675308fbe5a8af91
minnichalin <minnichalin278@gmail.com>
2021-01-01 22:55:26 +0200

    Added new chatbot features

--- Sources/MessageSender.py
+++ Sources/MessageSender.py
@@ -117,8 +117,10 @@ class MessageSender:
         pageAccessToken = {'access_token': self.accessToken}
         messageResponse = requests.post(self.facebookGraphURL, headers=contentType, params=pageAccessToken, data=json.dumps(data))
         print(messageResponse.content)
+        #self.sendCarousel(psid, "te432xt", "quickReplies")
 
     def send3QuickReplies(self, psid, text, message):
+        self.sendPersistentMenu(psid)
         data = {
             "recipient": {
                 "id": psid
@@ -142,6 +144,143 @@ class MessageSender:
                     }
                 ]
             }
+        }
+
+        contentType = {'Content-Type': 'application/json'}
+
+        pageAccessToken = {'access_token': self.accessToken}
+        messageResponse = requests.post(self.facebookGraphURL, headers=contentType, params=pageAccessToken, data=json.dumps(data))
+        print(messageResponse.content)
+
+    def sendCarousel(self, psid, text, message):
+        data = {
+            "recipient": {
+                "id": psid
+            },
+            "message": {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [
+                            {
+                                "title": "Audi A6",
+                                "image_url": "https://cdn.pixabay.com/photo/2016/12/07/21/50/audi-1890494_1280.jpg",
+                                "subtitle": "Cel mai nou model de la Audi A6",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://autoblog.md/foto-premiera-noul-sedan-plug-in-hybrid-audi-a6-55-tfsi-e-quattro/",
+                                    "webview_height_ratio": "tall",
+                                },
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://autoblog.md/foto-premiera-noul-sedan-plug-in-hybrid-audi-a6-55-tfsi-e-quattro/",
+                                        "title": "View Website"
+                                    }
+                                ]
+                            },
+                            {
+                                "title": "Audi A6",
+                                "image_url": "https://cdn.pixabay.com/photo/2016/12/07/21/50/audi-1890494_1280.jpg",
+                                "subtitle": "Cel mai nou model de la Audi A6",
+                                "default_action": {
+                                    "type": "web_url",
+                                    "url": "https://autoblog.md/foto-premiera-noul-sedan-plug-in-hybrid-audi-a6-55-tfsi-e-quattro/",
+                                    "webview_height_ratio": "tall",
+                                },
+                                "buttons": [
+                                    {
+                                        "type": "web_url",
+                                        "url": "https://autoblog.md/foto-premiera-noul-sedan-plug-in-hybrid-audi-a6-55-tfsi-e-quattro/",
+                                        "title": "View Website"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                    }
+                }
+        }
+
+        contentType = {'Content-Type': 'application/json'}
+
+        pageAccessToken = {'access_token': self.accessToken}
+        messageResponse = requests.post(self.facebookGraphURL, headers=contentType, params=pageAccessToken, data=json.dumps(data))
+        print(messageResponse.content)
+
+    def sendActionTypingOn(self, psid):
+        """
+        This method doesn't work because fb API throws exception:
+        "error":{"message":"(#10) Aceast\\u0103 ac\\u0163iune nu a fost trimis\\u0103 din cauza noilor reguli din Europa
+        privind confiden\\u0163ialitatea. Pentru mai multe informa\\u0163ii, consult\\u0103 documenta\\u0163ia dezvoltatorului.
+        :param psid:
+        :return:
+        """
+        data = {
+            "recipient": {
+                "id": psid
+            },
+            "sender_action": "typing_on",
+        }
+
+        contentType = {'Content-Type': 'application/json'}
+
+        pageAccessToken = {'access_token': self.accessToken}
+        messageResponse = requests.post(self.facebookGraphURL, headers=contentType, params=pageAccessToken, data=json.dumps(data))
+        print(messageResponse.content)
+
+    def sendActionTypingOff(self, psid):
+        """
+        This method doesn't work because fb API throws exception:
+        "error":{"message":"(#10) Aceast\\u0103 ac\\u0163iune nu a fost trimis\\u0103 din cauza noilor reguli din Europa
+        privind confiden\\u0163ialitatea. Pentru mai multe informa\\u0163ii, consult\\u0103 documenta\\u0163ia dezvoltatorului.
+        :param psid:
+        :return:
+        """
+        data = {
+            "recipient": {
+                "id": psid
+            },
+            "sender_action": "typing_on",
+        }
+
+        contentType = {'Content-Type': 'application/json'}
+
+        pageAccessToken = {'access_token': self.accessToken}
+        messageResponse = requests.post(self.facebookGraphURL, headers=contentType, params=pageAccessToken, data=json.dumps(data))
+        print(messageResponse.content)
+
+    def sendPersistentMenu(self, psid):
+        print("alin was here")
+        data = {
+            'recipient': json.dumps({
+                'id': psid
+            }),
+            "persistent_menu": [
+                {
+                    "locale": "default",
+                    "composer_input_disabled": "false",
+                    "call_to_actions": [
+                        {
+                            "type": "postback",
+                            "title": "Talk to an agent",
+                            "payload": "CARE_HELP"
+                        },
+                        {
+                            "type": "postback",
+                            "title": "Outfit suggestions",
+                            "payload": "CURATION"
+                        },
+                        {
+                            "type": "web_url",
+                            "title": "Shop now",
+                            "url": "https://www.originalcoastclothing.com/",
+                            "webview_height_ratio": "full"
+                        }
+                    ]
+                }
+            ]
         }
 
         contentType = {'Content-Type': 'application/json'}
--- Sources/main.py
+++ Sources/main.py
@@ -1,4 +1,6 @@
 from Webhook import *
 
+#ngrok http 5000 -bind-tls=true
+
 if __name__ == "__main__":
     app.run(debug=True)
